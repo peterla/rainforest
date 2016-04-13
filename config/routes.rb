3c3a2d62b57b871888c62d21d6c5722210b307be
Giang La <peter.la@peterla.com>
2016-04-12 21:05:28 -0400

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products
+  resources :users, only: [:new, :create]
+
+  # get 'users/new'
+  #
+  # get 'users/create'
 
   # get 'products/index'
   #
